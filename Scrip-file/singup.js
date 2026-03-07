0d5773c37139a26065cc1ae486a66b733696cada
mehidealways <mehidehasanmilon444@.com>
2026-03-08 00:50:20 +0600

    Initialize all setup and start my project and add some js functionlaty

---
+++ Scrip-file/singup.js
@@ -0,0 +1,17 @@
+const singUpBtn = document
+  .getElementById('btn-singup')
+  .addEventListener('click', () => {
+    const singupUserName = document.getElementById('singupUserName').value;
+    console.log('Name', singupUserName);
+    const singupUserPass = document.getElementById('singupUserPass').value;
+    console.log('Pass', singupUserPass);
+    
+    if (singupUserName == 'admin' && singupUserPass == 'admin123') {
+      
+      alert('Login Successful');
+      window.location.assign('index.html');
+    } else {
+      return 'Invalid username or password';
+    }
+
+  });
